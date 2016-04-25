86bedd761e1792419ffc5778e3cb579316a50381
hqzxzb <hqzxzb@163.com>
2016-04-25 10:14:22 +0800

    修改tab空格数并重新格式化代码

--- lua/access_proxy.lua
+++ lua/access_proxy.lua
@@ -6,12 +6,12 @@ local shared_data = ngx.shared.shared_data;
 
 --判断是否启用缓存
 if ifCache then
-		--通过共享内存获取代理地址
-		local shared_data_proxy = shared_data:get(ngx.var.root_path);
-		if ( shared_data_proxy ~= nil and shared_data_proxy ~= "" ) then 
-				ngx.var.dynamic_proxy_path = shared_data_proxy;
-				return ;
-		end
+	--通过共享内存获取代理地址
+	local shared_data_proxy = shared_data:get(ngx.var.root_path);
+	if ( shared_data_proxy ~= nil and shared_data_proxy ~= "" ) then 
+		ngx.var.dynamic_proxy_path = shared_data_proxy;
+		return ;
+	end
 end
 
 --加载redis模块
@@ -24,13 +24,13 @@ local port = 6379;
 
 --关闭redis连接
 local function close_redis(red)
-    if not red then
-        return ;
-    end
-    local ok, err = red:close();
-    if not ok then
-        ngx.say("close redis error : ", err);
-    end
+	if not red then
+		return ;
+	end
+	local ok, err = red:close();
+	if not ok then
+		ngx.say("close redis error : ", err);
+	end
 end
 
 --创建redis实例
@@ -40,27 +40,27 @@ red:set_timeout(15000);
 --建立redis连接
 local ok, err = red:connect(ip, port);
 if not ok then
-    ngx.say("connect to redis error : ", err);
-    close_redis(red);
+	ngx.say("connect to redis error : ", err);
+	close_redis(red);
 end
 
 --调用API获取数据
 local resp, err = red:get(ngx.var.root_path);
 if not resp then
-    ngx.say("get [", ngx.var.root_path, "] error : ", err);
-    close_redis(red);
+	ngx.say("get [", ngx.var.root_path, "] error : ", err);
+	close_redis(red);
 end
 
 --得到的数据为空处理
 if resp == ngx.null then
-		--设定默认值
-    resp = "http://127.0.0.1:" .. ngx.var.server_port .. "/errorproxy"; 
+	--设定默认值
+	resp = "http://127.0.0.1:" .. ngx.var.server_port .. "/errorproxy"; 
 else 
-		--判断是否启用全局缓存
-		if ifCache then 
-				--写入全局共享变量
-				shared_data:set(ngx.var.root_path, resp);
-		end
+	--判断是否启用全局缓存
+	if ifCache then 
+		--写入全局共享变量
+		shared_data:set(ngx.var.root_path, resp);
+	end
 end
 close_redis(red);
 
