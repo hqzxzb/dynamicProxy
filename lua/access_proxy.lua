ff4ab405cd8f89187d05ba90ee47f7348d73f98f
hqzxzb <hqzxzb@163.com>
2016-04-25 16:25:24 +0800

    修改配置数据位置，统一写入init.lua初始化脚本中

--- lua/access_proxy.lua
+++ lua/access_proxy.lua
@@ -1,14 +1,11 @@
 --Nginx日志写入
 local log = ngx.log;
 
---是否启用全局缓存
-local ifCache = false;
-
 --获取全局共享内存
 local shared_data = ngx.shared.shared_data;
 
 --判断是否启用缓存
-if ifCache then
+if if_cache_proxy_data then
 	--通过共享内存获取代理地址
 	local shared_data_proxy = shared_data:get(ngx.var.root_path);
 	if ( shared_data_proxy ~= nil and shared_data_proxy ~= "" ) then 
@@ -43,9 +40,9 @@ if proxy_url == ngx.null then
 	proxy_url = "http://127.0.0.1:" .. ngx.var.server_port .. "/errorproxy"; 
 else 
 	--判断是否启用全局缓存
-	if ifCache then 
+	if if_cache_proxy_data then 
 		--写入全局共享变量
-		shared_data:set(ngx.var.root_path, proxy_url);
+		shared_data:set(ngx.var.root_path, proxy_url, proxy_shared_cache_timeout);
 	end
 end
 
--- lua/init.lua
+++ lua/init.lua
@@ -1,4 +1,30 @@
---初始化处理
+--初始化
+
+-----------------配置-----------------Start
+
+--Redis服务
+redis_config = {
+	--IP
+	ip = "127.0.0.1",
+	--Port
+	port = 6379;
+	--链接超时时间（毫秒）
+	timeout = 15000;
+};
+
+
+--动态代理数据内存缓存
+--是否启用Nginx全局共享内存缓存代理数据（true/false）
+if_cache_proxy_data = true;
+--代理数据内存缓存失效时间（秒）--失效后重新从后台服务器获取
+proxy_shared_cache_timeout = 10;
+
+-----------------配置-----------------End
+
+
+
+
+-----------------初始化方法-----------------Start
 
 --获取日志输出
 local log = ngx.log;
@@ -6,20 +32,14 @@ local log = ngx.log;
 --加载Redis模块
 local redis = require("resty.redis");
 
---设定Redis连接参数
-local redis_ip = "127.0.0.1";
-local redis_port = 6379;
---连接超时时间（毫秒）
-local redis_timeout = 15000;
-
 --设定Redis连接全局获取方法
 function getRedisConnect()
 	--创建redis连接实例
 	local red = redis:new();
 	--设置redis连接超时（毫秒）
-	red:set_timeout(redis_timeout);
+	red:set_timeout(redis_config.timeout);
 	--建立redis连接
-	local ok, err = red:connect(redis_ip, redis_port);
+	local ok, err = red:connect(redis_config.ip, redis_config.port);
 	if not ok then
 		--ngx.say("connect to redis error : ", err);
 		log(ngx.ERR, "connect to redis error : ", err);
@@ -42,3 +62,5 @@ function close_redis(red)
 		return ;
 	end
 end
+
+-----------------初始化方法-----------------End
